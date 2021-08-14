7a1fc5cedb7cb1e5e2433404e9799d9fe964e3b1
Naoki Nishizawa <nk.nishizawa@gmail.com>
2021-08-14 12:33:52 +0900

    WIP

--- renderer.js
+++ renderer.js
@@ -3,6 +3,10 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import { Snow }  from  './snow.js'
 const Common = require("./lib/common.js")
+const Colormap   = require("./lib/colormap.js")
+
+const colormap   = new Colormap('white')
+colormap.setBlackRate(0.0)
 
 function randomRange(min, max) {
 	return ((Math.random()*(max-min)) + min); 
@@ -20,7 +24,7 @@ let renderer, scene, camera, stats
 let mouseX = 0
 let mouseY = 0
 
-let particles = []; 
+let snow
 
 init()
 animate()
@@ -33,35 +37,27 @@ function init() {
 
 	scene = new THREE.Scene();
 
-	camera = new THREE.PerspectiveCamera( 75, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
-	camera.position.z = 1000
+	camera = new THREE.PerspectiveCamera(90, SCREEN_WIDTH / SCREEN_HEIGHT, 1, 10000 );
+	camera.position.z = 2000
 	scene.add(camera);
 
-  let particleImage = new Image();
-  particleImage.src = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/82015/snowflake.png'; 
-  let material = new THREE.ParticleBasicMaterial( { 
-    color: 0xffffff, 
-    side: THREE.DoubleSide, 
-    //map: new THREE.Texture(particleImage) 
-  } );
-		
-	for (var i = 0; i < 3000; i++) {
-
-		const particle = new Snow( material )
-		scene.add( particle.sprite );
-		
-		particles.push(particle); 
-	}
+  snow= new Snow(10000, -2000, 2000)
+  scene.add( snow.flakes )
 
 	container.appendChild( renderer.domElement );
 
-	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-	document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-	document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+	//document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	//document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+	//document.addEventListener( 'touchmove', onDocumentTouchMove, false );
   window.addEventListener( 'resize', onWindowResize )
 
   stats = new Stats();
   document.body.appendChild( stats.dom );
+
+
+  const controls = new OrbitControls( camera, renderer.domElement );
+  //controls.target.set( 0, 10, 0 );
+  controls.update();
 
 }
 
@@ -109,27 +105,79 @@ function animate() {
 
 function render() {
 
-  for (let i = 0; i < particles.length; i++)	{
+  snow.updatePhysics()
 
-    const particle = particles[i]; 
-    particle.updatePhysics(); 
-
-    const p = particle.position
-
-    if      ( p.y < -1000 ) p.y += 2000
-
-    if      ( p.x >  1000 ) p.x -= 2000
-    else if ( p.x < -1000 ) p.x += 2000
-
-    if      ( p.z > 1000  ) p.z -= 2000
-    else if ( p.z < -1000 ) p.z += 2000
-
-  }
-
-	camera.position.x += (   mouseX - camera.position.x ) * 0.05
-	camera.position.y += ( - mouseY - camera.position.y ) * 0.05
-	camera.lookAt(scene.position)
+	//camera.position.x += (   mouseX - camera.position.x ) * 0.05
+	//camera.position.y += ( - mouseY - camera.position.y ) * 0.05
+	//camera.lookAt(scene.position)
 
 	renderer.render(scene, camera)		
   stats.update()
 }
+
+//looks for key presses and logs them
+document.body.addEventListener("keydown", function(e) {
+  console.log(`key: ${e.key}`);
+
+  switch(true) {
+    case e.key == '0':
+      colormap.set('black')
+      break
+    case e.key == '1':
+      colormap.set('white')
+      break
+    case e.key == '2':
+      colormap.set('blue-black')
+      break
+    case e.key == '3':
+      colormap.set('aqua-black')
+      break
+    case e.key == '4':
+      colormap.set('green-black')
+      break
+    case e.key == '5':
+      colormap.set('yellow-black')
+      break
+    case e.key == '6':
+      colormap.set('red-black')
+      break
+    case e.key == '7':
+      colormap.set('rose-black')
+      break
+    case e.key == '8':
+      colormap.set('purple-black')
+      break
+    case e.key == 'm':
+      colormap.set('mix-black')
+      break
+    case e.key == 'p':
+      window.api.send('竜とそばかすの姫_歌よ_Belle_中村佳穂.mp3', 'music')
+      break
+
+    case e.key == "b":
+      {
+        let b = Math.cbrt(colormap.getBlackRate())
+        b += 0.1
+        b = b ** 3
+        colormap.setBlackRate(b)
+      }
+      break
+
+    case e.key == "w":
+      {
+
+        let b = Math.cbrt(colormap.getBlackRate())
+        b -= 0.1
+        b = b >= 0 ? b ** 3 : 0
+        colormap.setBlackRate(b)
+      }
+      break
+
+    default:
+      break
+  }
+
+  snow.changeColor(colormap.choose())
+});
+
+
--- snow.js
+++ snow.js
@@ -7,42 +7,101 @@ const TO_RADIANS = Math.PI/180.0
 
 export class Snow {
 
-  constructor(material) {
-    this.sprite = new THREE.Sprite(material)
+  constructor(num, rangeMin = -1000, rangeMax = 1000) {
+    this.num = num
+    this.rangeMin = rangeMin
+    this.rangeMax = rangeMax
 
-    this.position = new THREE.Vector3(
-      Math.random() * 2000 - 1000,
-      Math.random() * 2000 - 1000,
-      Math.random() * 2000 - 1000
-    )
+    const positions = new Float32Array(this.num * 3)
 
-    this.sprite.position.set(
-      this.position.x,
-      this.position.y,
-      this.position.z
-    ) 
-    this.sprite.scale.set(10, 10, 10)
-    this.velocity = new THREE.Vector3(0, -8, 0)
+    for (let i = 0; i < this.num; i++) {
+      const x = Common.randomReal(this.rangeMin, this.rangeMax) 
+      const y = Common.randomReal(this.rangeMin, this.rangeMax)
+      const z = Common.randomReal(this.rangeMin, this.rangeMax)
 
-    this.rotateX(this.velocity, Common.random(-45,  45) * TO_RADIANS) 
-    this.rotateY(this.velocity, Common.random(  0, 360) * TO_RADIANS)
+      positions[i * 3 + 0] = x
+      positions[i * 3 + 1] = y
+      positions[i * 3 + 2] = z
+    }
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute( 'position', new THREE.BufferAttribute(positions, 3))
 
-    this.gravity = new THREE.Vector3(0,0,0); 
-    this.drag = 1
+    const fileName = `img/snowflake.png`
+    const texture  = new THREE.TextureLoader().load( fileName )
+
+    const material = new THREE.PointsMaterial({
+      color: 0xffffff, 
+      size: 20, 
+      map: texture, 
+      transparent: true, 
+      //blending: THREE.AdditiveBlending, 
+      //depthTest: false, 
+      //transparent: true 
+    });
+
+
+    this.flakes = new THREE.Points( geometry, material );
+
+    this.velocities = []
+    this.gravities  = []
+    this.drags      = []
+
+    for (let i = 0; i < this.num; i++) {
+      const v = new THREE.Vector3(0, -8, 0)
+      const g = new THREE.Vector3(0,0,0)
+      const d = 1
+
+      this.rotateX(v, Common.random(-45,  45) * TO_RADIANS) 
+      this.rotateY(v, Common.random(  0, 360) * TO_RADIANS)
+
+      this.velocities.push(v)
+      this.gravities.push(g)
+      this.drags.push(d)
+    }
+
   }
 
-  updatePhysics() {
-    this.velocity.multiplyScalar(this.drag)
-    this.velocity.add(this.gravity )
-    this.position.add(this.velocity)
-
-    this.sprite.position.set(
-      this.position.x,
-      this.position.y,
-      this.position.z
-    ) 
+  changeColor(c) {
+    this.flakes.material.color.set(c)
   }
 
+  updatePhysics(color) {
+
+
+    const positions = this.flakes.geometry.attributes.position.array
+
+    const range = this.rangeMax - this.rangeMin
+    for (let i = 0; i < this.num; i++) {
+      const v = this.velocities[i]
+      const g = this.gravities[i]
+      const d = this.drags[i]
+
+      v.multiplyScalar(d)
+      v.add(g)
+
+      let x = positions[i * 3 + 0]
+      let y = positions[i * 3 + 1]
+      let z = positions[i * 3 + 2]
+
+      x += v.x
+      y += v.y
+      z += v.z
+
+      if      ( y < this.rangeMin ) y += range
+
+      if      ( x > this.rangeMax ) x -= range
+      else if ( x < this.rangeMin ) x += range
+
+      if      ( z > this.rangeMax ) z -= range
+      else if ( z < this.rangeMin ) z += range
+
+      positions[i * 3 + 0] = x 
+      positions[i * 3 + 1] = y 
+      positions[i * 3 + 2] = z 
+    }
+
+    this.flakes.geometry.attributes.position.needsUpdate = true;
+  }
 
   rotateX(v, angle) {
     const axis = new THREE.Vector3(1, 0, 0)
@@ -62,3 +121,6 @@ export class Snow {
     return v
   }
 }
+
+
+
